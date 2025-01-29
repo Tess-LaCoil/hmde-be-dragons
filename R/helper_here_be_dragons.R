3772bebfa3c17a549c42a589f330e4f6008971b1
Tess-LaCoil <tesslacoil@protonmail.com>
2025-01-29 13:54:02 +1100

    Added code to run log-N prior on beta_c for bimodality result.

--- R/helper_here_be_dragons.R
+++ R/helper_here_be_dragons.R
@@ -36,8 +36,49 @@ fit_affine_model <- function(run_no, step_size, obs_data_frame,
   return(temp)
 }
 
+#Fit affine model
+fit_old_affine_model <- function(run_no, step_size, obs_data_frame,
+                             int_method,
+                             prior_pars_ind_const = c(1,2),
+                             prior_pars_ind_beta_1 = c(0,2),
+                             affine_model_old){
+
+  print(paste0("Run: ", run_no, " step size: ", step_size,
+               " Int method: ", int_method))
+  #Run the model
+
+  rstan_data <- hmde_model("affine_single_ind") |>
+    hmde_assign_data(n_obs = nrow(obs_data_frame),
+                     y_obs = obs_data_frame$y_obs,
+                     obs_index = obs_data_frame$obs_index,
+                     time = obs_data_frame$time,
+                     y_bar = mean(obs_data_frame$y_obs),
+                     step_size = step_size,
+                     int_method = int_method,
+                     prior_pars_ind_const = prior_pars_ind_const,
+                     prior_pars_ind_beta_1 = prior_pars_ind_beta_1)
+  suppressWarnings(
+    fit <- sampling(affine_model_old, rstan_data,
+                    chains = 1, cores = 1, iter = 2000)
+  )
+  fit@model_name <- "affine_single_ind"
+
+  #Extract parameter estimates
+  ests <- hmde_extract_estimates(fit = fit,
+                                 input_measurement_data = obs_data_frame)
+
+  temp <- tibble(
+    run = i,
+    step_size = step_size,
+    beta_0 = ests$individual_data$ind_beta_0_mean,
+    beta_1 = ests$individual_data$ind_beta_1_mean
+  )
+
+  return(temp)
+}
+
 opt_init_grid <- function(rstan_data, beta_0_vals, beta_1_vals,
-                          verbose = FALSE, alg = "LBFGS"){
+                          verbose = FALSE, alg = "LBFGS", affine_model){
   #Grid
   inits <- expand.grid(ind_beta_0 = beta_0_vals,
                        ind_beta_1 = beta_1_vals) %>%
@@ -53,7 +94,8 @@ opt_init_grid <- function(rstan_data, beta_0_vals, beta_1_vals,
                              init_vec = inits[i,],
                              run_no = i,
                              verbose = verbose,
-                             alg = alg)
+                             alg = alg,
+                             affine_model)
 
     print(paste0("Runtime: ", round(temp$est_tibble_temp$runtime, digits = 4), " minutes."))
 
@@ -68,7 +110,7 @@ opt_init_grid <- function(rstan_data, beta_0_vals, beta_1_vals,
 }
 
 opt_affine_model <- function(rstan_data, init_vec = NULL, run_no,
-                             verbose = FALSE, alg = "LBFGS"){
+                             verbose = FALSE, alg = "LBFGS", affine_model){
   if(!is.null(init_vec)){
     init <- list(ind_y_0 = rstan_data$y_obs[1],
        ind_const = init_vec$ind_const,
